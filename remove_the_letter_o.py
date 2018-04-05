48afead585fb1391f31fd6ce9286a6558af991e1
kkamagwi <smolyaevaekaterina@gmail.com>
2018-04-05 18:01:42 +0600

    add letter replacer

---
+++ remove_the_letter_o.py
@@ -0,0 +1,11 @@
+"""
+Create a function to remove all of the letters "o" in a string.
+There's no need to worry about uppercase letters.
+
+Examples:
+"hello world" --> "hell wrld"
+"bono" -- "bn"
+"""
+
+def removeO(string):
+    return string.replace('o', '')
